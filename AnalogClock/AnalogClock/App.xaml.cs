fab1669493594ef90dde5311765d57631e0a55dc
Геннадий <gemiyur@live.ru>
2024-11-25 10:16:29 +0300

    Создал решение и проект AnalogClock.

---
+++ AnalogClock/AnalogClock/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AnalogClock
+{
+    /// <summary>
+    /// Класс приложения.
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
