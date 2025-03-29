2a7d46f529356b2db24f27d875ed5b77402eaf6d
Геннадий <gemiyur@live.ru>
2025-03-29 12:51:16 +0300

    Преобразователи времени в угол часов.

---
+++ AnalogClock/AnalogClock/Controls/ClockConverters.cs
@@ -0,0 +1,76 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace AnalogClock.Controls;
+
+/// <summary>
+/// Преобразует миллисекунды в угол часов.
+/// </summary>
+[ValueConversion(typeof(DateTime), typeof(double))]
+public class MillisecondsConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        DateTime date = (DateTime)value;
+        return (date.Second * 6) + (date.Millisecond * 0.006);
+    }
+
+    public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
+
+/// <summary>
+/// Преобразует секунды в угол часов.
+/// </summary>
+[ValueConversion(typeof(DateTime), typeof(int))]
+public class SecondsConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        DateTime date = (DateTime)value;
+        return date.Second * 6;
+    }
+
+    public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
+
+/// <summary>
+/// Преобразует минуты в угол часов.
+/// </summary>
+[ValueConversion(typeof(DateTime), typeof(int))]
+public class MinutesConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        DateTime date = (DateTime)value;
+        return (date.Minute * 6) + (date.Second * 0.1);
+    }
+
+    public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
+
+/// <summary>
+/// Преобразует часы в угол часов.
+/// </summary>
+[ValueConversion(typeof(DateTime), typeof(int))]
+public class HoursConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        DateTime date = (DateTime)value;
+        return (date.Hour * 30) + (date.Minute / 2);
+    }
+
+    public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
